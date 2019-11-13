44ae8b1c071c37b8a8f5ea2eabad8ebf09d6e47b
majdbassoumi <majd@souktel.org>
2019-11-13 16:09:39 +0200

    fixes in migrations

--- src/SouktelMessageBrokerServiceProvider.php
+++ src/SouktelMessageBrokerServiceProvider.php
@@ -19,8 +19,8 @@ class SouktelMessageBrokerServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../database/migrations/create_succeeded_consumed_messages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_' . config('souktel-message-broker.database.succeeded_consumed_messages') . '_table.php'),
             __DIR__ . '/../database/migrations/create_failed_consumed_messages_table.php.stub'    => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_' . config('souktel-message-broker.database.failed_consumed_messages') . '_table.php'),
-            __DIR__ . '/../database/migrations/create_succeeded_publised_messages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_' . config('souktel-message-broker.database.succeed_published_messages') . '_table.php'),
-            __DIR__ . '/../database/migrations/create_failed_publised_messages_table.php.stub'    => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_' . config('souktel-message-broker.database.failed_published_messages') . '_table.php'),
+            __DIR__ . '/../database/migrations/create_succeeded_published_messages_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_' . config('souktel-message-broker.database.succeed_published_messages') . '_table.php'),
+            __DIR__ . '/../database/migrations/create_failed_published_messages_table.php.stub'    => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_' . config('souktel-message-broker.database.failed_published_messages') . '_table.php'),
         ], 'migrations');
 
     }
@@ -38,12 +38,12 @@ class SouktelMessageBrokerServiceProvider extends ServiceProvider
             return new MessageBroker;
         });
 
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/souktel-message-broker.php', 'souktel-message-broker'
+        );
 
         $this->mergeConfigFrom(
             __DIR__ . '/../config/amqp.php', 'amqp'
-        );
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/souktel-message-broker.php', 'souktel-message-broker'
         );
 
         $this->app->bind('command.messages:consume', MessageConsumer::class);
